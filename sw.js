13c8191000d84dbb8bbc02b83e2e4a5ed6b7eb23
PatP3 <198839017@collegeahuntsic.qc.ca>
2022-02-03 10:34:24 -0500

    sw.js et index.js

---
+++ sw.js
@@ -0,0 +1,53 @@
+var cacheName = 'cache-tp2-v1';
+var filesToCache = [
+    '/pwa-tp2/',
+    '/pwa-tp2/index.html',
+    '/pwa-tp2/index.js',
+    '/pwa-tp2/bootstarp-5.1.3-dist/',
+    '/pwa-tp2/bootstarp-5.1.3-dist/css/',
+    '/pwa-tp2/bootstrap-5.1.3-dist/css/bootstrap.min.css',
+    '/pwa-tp2/bootstarp-5.1.3-dist/js/',
+    '/pwa-tp2/bootstrap-5.1.3-dist/js/bootstrap.min.js',
+    '/pwa-tp2/img/',
+    '/pwa-tp2/img/manoir.png',
+    '/pwa-tp2/img/niagara.png',
+    '/pwa-tp2/img/tesla.png',
+    '/pwa-tp2/favicon.ico'
+]
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Installation');
+
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        }).then(function() {
+            return self.skipWaiting();
+        })
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    console.log('[Service Worker] Activation');
+});
+
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request)
+        .then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        fetch(e.request).catch(function() {
+            return caches.match(e.request);
+        })
+    );
+});
+
+// onmessage = function(e) {
+//     console.log('[Service Worker] On Message: ', e);
+// }
