5d97b31a3ed8f10637aeb633ffbc50226f5b6c19
PatP3 <198839017@collegeahuntsic.qc.ca>
2022-02-08 15:44:36 -0500

    route 'sw.js'

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js').then(function(reg) {
+    navigator.serviceWorker.register('sw.js').then(function(reg) {
         // Enregistrement fonctionne
         console.log('Enregistrement du Service Worker');
     }).catch(function(error) {
