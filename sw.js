ccd3e35bf2f1eb54bf181d86c25da15960aff8e8
PatP3 <198839017@collegeahuntsic.qc.ca>
2022-02-09 10:20:13 -0500

    commit 10:20

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 console.log('il est dans le sw');
 var cacheName = 'pwa-tp2-v1';
 var filesToCache = [
-    '/pwa-tp2/.',
+    '/pwa-tp2/',
     '/pwa-tp2/index.html',
     '/pwa-tp2/index.js',
     '/pwa-tp2/bootstrap-5.1.3-dist/',
